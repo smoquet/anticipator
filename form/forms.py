f55f2762ea63baf7892d71a2bada9058290ecf19
smoquet <sr_moquette@yahoo.com>
2016-04-19 23:47:32 +0200

    form init

--- form/forms.py
+++ form/forms.py
@@ -1,4 +1,10 @@
 from django import forms
 
+
+# creer hier het formulier, dat instantiated wordt in views.py
+# daarna in index.html gestopt
 class NameForm(forms.Form):
     lineup = forms.CharField(label='Lineup', max_length=200)
+    playlist_name = forms.CharField(label='Playlist Name', max_length=200)
+    sort = forms.BooleanField(label='Sort', required = False, initial = True )
+    public = forms.BooleanField(label='Public playlist', required=False, initial = True)
