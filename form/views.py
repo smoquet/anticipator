dd54f5f3e5071df53fb39d0fc601e242c30584fb
smoquet <sr_moquette@yahoo.com>
2016-07-26 01:26:09 +0200

    fixes search for char bug

--- form/views.py
+++ form/views.py
@@ -195,7 +195,8 @@ def victory(request):
         party_values = party.values()
         source = unicodetostring(party_values[0]['source'])
         source_id = unicodetostring(party_values[0]['source_id'])
-        lineup = unicodetostring(party_values[0]['line_up'])
+        lineup = helper.list_to_string_or_back(unicodetostring(party_values[0]['line_up']))
+        print 'lineup type', type(lineup)
         print 'bron = ' , source, source_id
 
 
@@ -232,7 +233,7 @@ def victory(request):
     source_id = unicodetostring(party_values[0]['source_id'])
     print 'bron = ' , source, source_id
 
-    lineup = unicodetostring(party_values[0]['line_up'])
+    lineup = helper.list_to_string_or_back(unicodetostring(party_values[0]['line_up']))
 
     '''
     Spotify happens below
